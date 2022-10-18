bbb5fcccbee7ac2b89b900a89be48f8a9f1e0797
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2022-10-17 23:17:08 -0300

    Update script.js

--- ex012/script.js
+++ ex012/script.js
@@ -4,10 +4,10 @@ function tabuada() {
     if(num.value.length == 0) {
         alert('Por favor, digite um número!')
     } else {
-        let n = Number(num.value) // Essa variável será o número que foi digitado
+        let n = Number(num.value)       // Essa variável será o número que foi digitado
         let c = 1
-        tab.innerHTML = ''
-        while (c <= 10) {
+        tab.innerHTML = ''             //Antes de começar a mostrar a próxima tabuada, limpa a área
+        while (c <= 10) {             //Enquanto o contador for menor/igual a 10
             let item = document.createElement('option') 
             item.text = `${n} x ${c} = ${n * c}`
             item.value = `tab ${c}`
@@ -16,3 +16,4 @@ function tabuada() {
         }
     }
 }
+
