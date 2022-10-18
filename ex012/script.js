2e67382e6c54bd5a13df387442e77d79d35acef6
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2022-10-17 23:32:26 -0300

    Update script.js

--- ex012/script.js
+++ ex012/script.js
@@ -8,12 +8,14 @@ function tabuada() {
         let c = 1
         tab.innerHTML = ''             //Antes de começar a mostrar a próxima tabuada, limpa a área
         while (c <= 10) {             //Enquanto o contador for menor/igual a 10
-            let item = document.createElement('option') 
-            item.text = `${n} x ${c} = ${n * c}`
+            let item = document.createElement('option')   //Criando um elemento option, de forma dinâmica...
+            item.text = `${n} x ${c} = ${n * c}`   //A parte de dentro do option
             item.value = `tab ${c}`
-            tab.appendChild(item)
+            tab.appendChild(item)      //Adicionar um elemento filho que vai ser o (item)
             c++
         }
     }
 }
 
+// linha 11: let item = adiciona elemento no select; 
+//...é o msmo de: <option value=""></option>
