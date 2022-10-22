c15f5551bb767344f1c67200f6e5177f78097254
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2022-10-21 23:25:26 -0300

    Update ex04.js

--- ex004/ex04.js
+++ ex004/ex04.js
@@ -2,7 +2,7 @@ var idade = 31
     console.log(`Você tem ${idade} anos!`)
 if(idade < 16 ) {
     console.log(`Você não vota`)
-} else if(idade < 18 || idade > 65) {
+} else if(idade < 18 || idade > 70) {
     console.log(`Voto opcional`)
 } else {
     console.log(`Voto obrigatório`)
