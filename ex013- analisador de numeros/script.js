83c134b8bfd736de3e30ff5c1031ce9ddf689632
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2022-10-24 18:51:00 -0300

    Update script.js

--- ex013- analisador de numeros/script.js
+++ ex013- analisador de numeros/script.js
@@ -32,4 +32,14 @@ function adicionar() {
     num.focus()
 }
 
+function finalizar() {
+    if(valores.length == 0) {
+         alert('Adicione valores antes de finalizar!')
+    } else {
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>Ao todo, temos ${tot} números cadastrados.</p>`
+    }
+}
 
