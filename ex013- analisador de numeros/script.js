2a53e7fee82159d3225d043b0348249cde197527
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2022-10-24 18:58:30 -0300

    Update script.js

--- ex013- analisador de numeros/script.js
+++ ex013- analisador de numeros/script.js
@@ -25,6 +25,7 @@ function adicionar() {
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado`
         lista.appendChild(item)
+        res.innerHTL += ''
     } else {
         alert('Valor inválido ou já encontrado na lista')           
     }
